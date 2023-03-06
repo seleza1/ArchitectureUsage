0aa85eca9c9de77fbfae33d148daa2461e81948d
Koposov Seryozha <koposov.serezha@gmail.com>
2023-03-06 13:20:46 +0300

    Initial Commit

---
+++ ArchitectureUsage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureUsage
+//
+//  Created by user on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
