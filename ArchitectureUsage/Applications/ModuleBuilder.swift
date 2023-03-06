7d66a33e6a2213410d23c8daed05a9aefcbdd4d8
Koposov Seryozha <koposov.serezha@gmail.com>
2023-03-06 14:08:21 +0300

    create inject

---
+++ ArchitectureUsage/Applications/ModuleBuilder.swift
@@ -0,0 +1,21 @@
+//
+//  ModuleBuilder.swift
+//  ArchitectureUsage
+//
+//  Created by user on 06.03.2023.
+//
+
+import UIKit
+
+protocol Builder {
+    static func createMain() -> UIViewController
+}
+
+class ModelBuilder: Builder {
+    static func createMain() -> UIViewController {
+        let person = Person(firstName: "Selezza", lastName: "crame")
+        let view = MainViewController()
+        let presenter = MainPresenter(view: view, person: person)
+        
+    }
+}
